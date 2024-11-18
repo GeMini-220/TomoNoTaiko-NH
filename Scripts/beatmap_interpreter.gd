82ee9d427f535bb2fcea945daad2b0d468290dcd
HarryS561 <2608771343@qq.com>
2024-11-18 15:52:25 -0800

    Merge Scoring System

--- Scripts/beatmap_interpreter.gd
+++ Scripts/beatmap_interpreter.gd
@@ -9,14 +9,18 @@ var note_index = 0
 var beatmap = {}
 @onready var conductor = $Conductor
 @onready var Global = $"/root/Global"
+@onready var score_display = $ScoreDisplay
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Conductor.play_with_beat_offset(0)		# can change offset
+	conductor.play_with_beat_offset(0)		# can change offset
 	Global.beat.connect(_on_Conductor_beat)
 	#Global.measure.connect(_on_Conductor_measure)
 	
+func _process(delta: float) -> void:
+	# Update the score display every frame
+	score_display.text = "Score: " + str(Global.score) + " Combo: " + str(Global.combo)
 	
 func _on_Conductor_beat(position):
 	var beatmap = StaticData.beatmapData["notes"]
--- Scripts/global.gd
+++ Scripts/global.gd
@@ -2,3 +2,25 @@ extends Node2D
 
 signal beat(position)
 signal measure(position)
+
+#Global score and combo
+var score: int = 0
+var combo: int = 0
+
+# Function to add points
+func add_score(points: int) -> void:
+	if combo == 0:
+		score += points
+	else:
+		score += points * combo
+	if combo <= 10:
+		combo += 1
+	print("Current Score: ", score, " | Combo: x", combo)
+
+#reset score(call when starting level)
+func reset_score() -> void:
+	score = 0
+	combo = 0
+
+func reset_combo()-> void:
+	combo = 0
